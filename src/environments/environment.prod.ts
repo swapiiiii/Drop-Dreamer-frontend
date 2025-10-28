f24196128e56c09167c34624c9e76507b15e82f3
swapiiiii <swapnilb921@gmail.com>
2025-10-28 16:01:41 +0530

    "Backend url corrected"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://drop-dreamer-backend-production.up.railway.app/users' // live backend
+  apiUrl: 'https://drop-dreamer-backend-production.up.railway.app'
 };
